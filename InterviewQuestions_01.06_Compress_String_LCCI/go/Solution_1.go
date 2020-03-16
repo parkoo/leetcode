7cb7e6bcbb78295cee8742c302b71f69749f5ed1
Crownt <jintianwang520@gmail.com>
2020-03-16 10:58:20 +0800

    IQ_01.06 go

---
+++ InterviewQuestions_01.06_Compress_String_LCCI/go/Solution_1.go
@@ -0,0 +1,35 @@
+package main
+
+import "strconv"
+
+// 双指针取连续字符
+// 时间复杂度：O(n)  空间复杂度：O(1)
+
+func compressString_1(S string) string {
+
+	if len(S)==0 {
+		return ""
+	}
+
+	str := ""
+	cnt := 1
+	i := 0
+	j := 1
+	
+	for ; j<len(S); j++ {
+		if S[i:i+1]==S[j:j+1]{
+			cnt++
+		}else {
+			str = str + S[i:i+1] + strconv.Itoa(cnt)
+			i = j
+			cnt = 1
+		}
+	}
+	str = str + S[i:i+1] + strconv.Itoa(cnt)
+
+	if len(str)<len(S){
+		return str
+	}
+
+	return S	
+}
