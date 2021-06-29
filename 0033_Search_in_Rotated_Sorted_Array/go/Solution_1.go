ceb1d3f10ca6d836209abc951641c7007f1376d0
jintian <jintian.wang01@liulishuo.com>
2021-06-29 21:33:30 +0800

    0033 go

---
+++ 0033_Search_in_Rotated_Sorted_Array/go/Solution_1.go
@@ -0,0 +1,41 @@
+package main
+
+// 二分查找
+// 时间复杂度：O(lgn)  空间复杂度：O(1)
+
+func search(nums []int, target int) int {
+	n := len(nums)
+	if n == 0 {
+		return -1
+	}
+
+	if n == 1 {
+		if nums[0] == target {
+			return 0
+		} else {
+			return -1
+		}
+	}
+
+	l, r := 0, n-1
+	for l <= r {
+		mid := l + (r-l)/2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] >= nums[0] { // mid 在旋转左半部分
+			if target >= nums[0] && target < nums[mid] { // target 在旋转左半部分，target < nums[mid] (即 target 在 nums[mid] 左边)
+				r = mid - 1
+			} else {
+				l = mid + 1
+			}
+		} else { // mid 在旋转右半部分
+			if target < nums[0] && target > nums[mid] { // target 在旋转右半部分，target > nums[mid] (即 target 在 nums[mid] 右边)
+				l = mid + 1
+			} else {
+				r = mid - 1
+			}
+		}
+	}
+
+	return -1
+}
