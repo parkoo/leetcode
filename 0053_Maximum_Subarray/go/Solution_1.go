4122561756c924ff8e856757bfce89af6e1b4420
jintian <jintian.wang520@gamil.com>
2021-10-20 23:02:58 +0800

    0053 go

---
+++ 0053_Maximum_Subarray/go/Solution_1.go
@@ -0,0 +1,31 @@
+package main
+
+// 动态规划
+// 时间复杂度：O(n)  空间复杂度：O(n)
+
+func maxSubArray_1(nums []int) int {
+	n := len(nums)
+	if n == 0 {
+		return 0
+	}
+
+	// dp[i]表示考虑到第i个位置时，可以得到的最大子序和
+	dp := make([]int, n)
+	dp[0] = nums[0]
+
+	maxSum := dp[0]
+	for i := 1; i < n; i++ {
+		dp[i] = max(nums[i], dp[i-1]+nums[i])
+		maxSum = max(maxSum, dp[i])
+	}
+
+	return maxSum
+}
+
+func max(i, j int) int {
+	if i > j {
+		return i
+	}
+
+	return j
+}
