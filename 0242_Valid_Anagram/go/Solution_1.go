c4ccc7cb37779e0b0233c075c585c3911153f2d3
jintian <jintian.wang520@gamil.com>
2021-10-26 23:30:21 +0800

    0242 go

---
+++ 0242_Valid_Anagram/go/Solution_1.go
@@ -0,0 +1,37 @@
+package main
+
+// 字符串 词频统计
+// 时间复杂度：O(n)  空间复杂度：O(1)
+
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+
+	ss, ts := []rune(s), []rune(t)
+	smap, tmap := make(map[rune]int, 0), make(map[rune]int, 0)
+
+	// 对t进行词频统计
+	for _, v := range ts {
+		if i, ok := tmap[v]; ok {
+			tmap[v] = i + 1
+		} else {
+			tmap[v] = 1
+		}
+	}
+
+	cnt := 0
+	for i := 0; i < len(ss); i++ {
+		if v, ok := smap[ss[i]]; ok {
+			smap[ss[i]] = v + 1
+		} else {
+			smap[ss[i]] = 1
+		}
+
+		if v, ok := tmap[ss[i]]; ok && smap[ss[i]] <= v {
+			cnt++
+		}
+	}
+
+	return cnt == len(t)
+}
