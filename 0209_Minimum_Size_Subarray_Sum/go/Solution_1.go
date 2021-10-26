d48b56ba10d252b16e8604d2b2c4cf9a0abc0705
jintian <jintian.wang520@gamil.com>
2021-10-26 21:06:29 +0800

    0209 go

---
+++ 0209_Minimum_Size_Subarray_Sum/go/Solution_1.go
@@ -0,0 +1,28 @@
+package main
+
+// 双指针  滑动窗口
+// 时间复杂度：O(n)  空间复杂度：O(1)
+
+func minSubArrayLen(target int, nums []int) int {
+	cnt := len(nums) + 1
+	i, j := 0, -1 // 滑动串口为[i,j], j<i时表示窗口中无元素，j=i时表示窗口中有一个元素
+	sum := 0
+	for i < len(nums) {
+		if j < len(nums)-1 && sum < target { // 防止j越界
+			j++
+			sum += nums[j]
+		} else {
+			sum -= nums[i]
+			i++
+		}
+
+		if sum >= target && j-i+1 < cnt {
+			cnt = j - i + 1
+		}
+	}
+
+	if cnt == len(nums)+1 {
+		cnt = 0
+	}
+	return cnt
+}
