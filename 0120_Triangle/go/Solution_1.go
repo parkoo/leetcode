a034062b322654377309c20c9233d05bcfab1f95
jintian <jintian.wang520@gamil.com>
2021-11-14 21:55:20 +0800

    0120 go

---
+++ 0120_Triangle/go/Solution_1.go
@@ -0,0 +1,41 @@
+package main
+
+// 动态规划
+// 时间复杂度：O(mn)  空间复杂度：O(mn)
+
+func minimumTotal(triangle [][]int) int {
+	m := len(triangle)
+	n := len(triangle[m-1])
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+	dp[0][0] = triangle[0][0]
+
+	for i := 1; i < m; i++ {
+		// 第i行的第一个点只能由第i-1行的第一个点获取
+		dp[i][0] = dp[i-1][0] + triangle[i][0]
+		k := len(triangle[i])
+		for j := 1; j < k-1; j++ {
+			dp[i][j] = min(dp[i-1][j], dp[i-1][j-1]) + triangle[i][j]
+		}
+		// 第i行的最后一个点只能由上一行的最后一个点获取
+		dp[i][k-1] = dp[i-1][k-2] + triangle[i][k-1]
+	}
+
+	minValue := dp[m-1][0]
+	for i := 0; i < len(dp[m-1]); i++ {
+		if dp[m-1][i] < minValue {
+			minValue = dp[m-1][i]
+		}
+	}
+
+	return minValue
+}
+
+func min(i, j int) int {
+	if i < j {
+		return i
+	}
+	return j
+}
