b83b334914f41b71b3e9f2165dc7cfd1ffb489a0
jintian <jintian.wang520@gamil.com>
2021-11-29 23:42:50 +0800

    0092 go

---
+++ 0092_Reverse_Linked_List_II/go/Solution_1.go
@@ -0,0 +1,31 @@
+package main
+
+// 头插法 反转
+// 时间复杂度：O(n)  空间复杂度：O(1)
+
+// Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseBetween_1(head *ListNode, left int, right int) *ListNode {
+	dummyHead := new(ListNode)
+	dummyHead.Next = head
+
+	start := dummyHead
+	for i := 0; i < left-1; i++ {
+		start = start.Next
+	}
+
+	cur := start.Next
+	// 将cur后面的节点进行头插，头插后cur自动后移 不需要cur = cur.Next
+	for i := 0; i < right-left; i++ {
+		next := cur.Next
+		cur.Next = next.Next
+		next.Next = start.Next
+		start.Next = next
+	}
+
+	return dummyHead.Next
+}
