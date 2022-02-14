f8d74a0a378b1113237c2efd5d7937a71aad6876
jintian.wang <jintian.wang@mihoyo.com>
2022-02-14 23:22:58 +0800

    0055 go

---
+++ 0055_Jump_Game/go/Solution_1.go
@@ -0,0 +1,23 @@
+package main
+
+// 贪心法  反向查找
+// 时间复杂度：O(n) 空间复杂度：O(1)
+
+func canJump_1(nums []int) bool {
+	if len(nums) == 1 {
+		return true
+	}
+
+	curIndex := len(nums) - 1
+	for i := curIndex - 1; i >= 0; i-- {
+		if nums[i] >= curIndex-i {
+			if i == 0 {
+				return true
+			} else {
+				curIndex = i
+			}
+		}
+	}
+
+	return false
+}
