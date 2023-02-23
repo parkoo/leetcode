c3a6377cf54d75a47ef9e4da180714c765035ec1
jintian.wang <jintian.wang@mihoyo.com>
2023-02-23 22:44:44 +0800

    0003 go

--- 0003_Longest_Substring_Without_Repeating_Characters/go/Solution_1.go
+++ 0003_Longest_Substring_Without_Repeating_Characters/go/Solution_1.go
@@ -4,24 +4,19 @@ package main
 // 时间复杂度：O(n)  空间复杂度：O(min(m,n)),m表示字符集大小，n表示字符串大小
 
 func lengthOfLongestSubstring_1(s string) int {
-
 	start := 0
-	longest_length := 0
-	m := make(map[rune]int)
-
+	longestLength := 0
+	locMap := make(map[rune]int)
 	for i, character := range []rune(s) {
-		if index, ok := m[character]; ok && index>start {
+		if index, ok := locMap[character]; ok && index+1 > start {
 			start = index + 1
 		}
-
-		m[character] = i
-
 		length := i - start + 1
-
-		if length>longest_length {
-			longest_length = length
+		if length > longestLength {
+			longestLength = length
 		}
+		locMap[character] = i
 	}
 
-	return longest_length
+	return longestLength
 }
