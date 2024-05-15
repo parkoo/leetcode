66f082c2f23599c88e7a2e6fe7a918fe45de9223
jintian <jintianwang520@gmail.com>
2024-05-15 11:35:31 +0800

    0039 go

--- 0039_Combination_Sum/go/Solution_1.go
+++ 0039_Combination_Sum/go/Solution_1.go
@@ -5,8 +5,8 @@ package main
 func combinationSum_1(candidates []int, target int) [][]int {
 	res := make([][]int, 0)
 
-	var backtract func(start, target, curSum int, candidates, subArr []int)
-	backtract = func(start, target, curSum int, candidates, subArr []int) {
+	var backtract func(start, curSum int, candidates, subArr []int)
+	backtract = func(start, curSum int, candidates, subArr []int) {
 		if curSum == target {
 			arr := make([]int, len(subArr))
 			for i, v := range subArr {
@@ -20,12 +20,13 @@ func combinationSum_1(candidates []int, target int) [][]int {
 		for i := start; i < len(candidates); i++ {
 			if curSum+candidates[i] <= target {
 				subArr = append(subArr, candidates[i])
-				backtract(i, target, curSum+candidates[i], candidates, subArr)
+				// start传入i, 保证下层节点与当前节点从同一个位置开始，避免结果重复 (如：[2,2,3]、[2,3,2]、[3,2,2])
+				backtract(i, curSum+candidates[i], candidates, subArr)
 				subArr = subArr[:len(subArr)-1]
 			}
 		}
 	}
 
-	backtract(0, target, 0, candidates, []int{})
+	backtract(0, 0, candidates, []int{})
 	return res
 }
