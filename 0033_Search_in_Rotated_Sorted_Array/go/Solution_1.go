777e0b42fefa0ebdae95b2357d9903c7981ef4df
jintian <jintianwang520@gmail.com>
2024-06-21 20:58:58 +0800

    0033 go

--- 0033_Search_in_Rotated_Sorted_Array/go/Solution_1.go
+++ 0033_Search_in_Rotated_Sorted_Array/go/Solution_1.go
@@ -14,13 +14,13 @@ func search(nums []int, target int) int {
 		mid := l + (r-l)/2
 		if nums[mid] == target {
 			return mid
-		} else if nums[mid] >= nums[0] { // mid 在旋转左半部分
+		} else if nums[mid] >= nums[0] { // mid 在旋转左半部分, 此时[l, mid] 为有序部分
 			if target >= nums[0] && target < nums[mid] { // target 在旋转左半部分，target < nums[mid] (即 target 在 nums[mid] 左边)
 				r = mid - 1
 			} else {
 				l = mid + 1
 			}
-		} else { // mid 在旋转右半部分
+		} else { // mid 在旋转右半部分 此时[mid, r] 为有序部分
 			if target < nums[0] && target > nums[mid] { // target 在旋转右半部分，target > nums[mid] (即 target 在 nums[mid] 右边)
 				l = mid + 1
 			} else {
