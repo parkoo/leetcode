6964be6d19490e05e177bb1ea30b3f4831b77fbf
jintian <jintianwang520@gmail.com>
2024-06-25 10:12:43 +0800

    0138 go

--- 0138_Copy_List_with_Random_Pointer/go/solution_3.go
+++ 0138_Copy_List_with_Random_Pointer/go/solution_3.go
@@ -12,7 +12,43 @@ type Node struct {
 }
 
 func copyRandomList_3(head *Node) *Node {
-	// TODO
+	if head == nil {
+		return nil
+	}
 
-	return nil
+	// 节点复制
+	cur := head
+	for cur != nil {
+		next := cur.Next
+		newNode := new(Node)
+		newNode.Val = cur.Val
+		cur.Next = newNode
+		newNode.Next = next
+		cur = cur.Next.Next
+	}
+
+	// 随机指针复制
+	cur = head
+	for cur != nil {
+		random := cur.Random
+		newNode := cur.Next
+		if random != nil { // 注意 random为空的情况
+			newNode.Random = random.Next
+		}
+		cur = cur.Next.Next
+	}
+
+	// 拆分
+	cur = head
+	newHead := cur.Next // 注意 head为空的情况
+	for cur != nil {
+		newNode := cur.Next
+		cur.Next = cur.Next.Next
+		if cur.Next != nil { // 注意 cur.Next为空的情况
+			newNode.Next = cur.Next.Next
+		}
+		cur = cur.Next
+	}
+
+	return newHead
 }
