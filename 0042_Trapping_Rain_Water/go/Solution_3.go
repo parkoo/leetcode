2a619bcf55517a2822d271df225a4f91efc523a9
jintian <jintianwang520@gmail.com>
2024-06-26 15:12:08 +0800

    0042 go

--- 0042_Trapping_Rain_Water/go/Solution_3.go
+++ 0042_Trapping_Rain_Water/go/Solution_3.go
@@ -1,37 +1,32 @@
 package main
 
-import (
-	"math"
-)
-
 // 双指针 对撞指针，每次根据情况,移动左指针或者右指针
 // 时间复杂度：O(n)  空间复杂度：O(１)
 
 func trap_3(height []int) int {
-
 	res := 0
 
-	left := 0
-	right := len(height)-1
+	leftMax, rightMax := height[0], height[len(height)-1]
+	left, right := 0, len(height)-1
 
-	max_l := 0
-	max_r := 0
-	for left<right {
-		if height[left]<height[right] {
-			if height[left]>max_l {
-				max_l = height[left]
-			} else {
-				res += max_l - height[left]
-			}
+	// 始终保证 leftMax >= height[left]、 rightMax >= height[right]
+	// 因为每次只移动短板一侧的指针，若 height[left] < height[right]，则一定有 leftMax < rightMax
+	// 若 height[left] < height[right]，则 leftMax >= height[left] < height[right] <= rightMax, 且 leftMax <= rightMax, 可计算left位置的水量
+	// 反之，计算right位置的水量
+	for left < right {
+		if height[left] > leftMax {
+			leftMax = height[left]
+		}
+		if height[right] > rightMax {
+			rightMax = height[right]
+		}
 
+		if height[left] < height[right] {
+			res += (leftMax - height[left])
 			left++
-		} else {
-			if height[right]>max_r {
-				max_r = height[right]
-			} else {
-				res += max_r - height[right]
-			}
 
+		} else {
+			res += (rightMax - height[right])
 			right--
 		}
 	}
