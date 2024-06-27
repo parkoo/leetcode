0441aea7a2f7f27314e45d032ef2c3cf1580cd65
jintian <jintianwang520@gmail.com>
2024-06-27 19:05:01 +0800

    0039 go

--- 0039_Combination_Sum/go/Solution_1.go
+++ 0039_Combination_Sum/go/Solution_1.go
@@ -1,32 +1,37 @@
 package main
 
-// 回溯法 组合问题
+// 思路: 回溯法 组合问题 元素可以重复使用  注意比较lc78(元素不可以重复使用)
+// 组合问题中 在回溯函数中需要传入起始位置，若数组中的元素可以重复使用，则向下层函数传入当前位置i
+// 若数组中的元素可以重复使用，则向下层函数传入当前位置i的下一个位置，即i+1
+// 这样可以避免结果集中出现重复结果
 
-func combinationSum_1(candidates []int, target int) [][]int {
+func combinationSum(candidates []int, target int) [][]int {
 	res := make([][]int, 0)
 
-	var backtract func(start, curSum int, candidates, subArr []int)
-	backtract = func(start, curSum int, candidates, subArr []int) {
-		if curSum == target {
-			arr := make([]int, len(subArr))
-			for i, v := range subArr {
-				arr[i] = v
-			}
-			res = append(res, arr)
-
+	var backtrack func(nums []int, sub []int, sum int, start int)
+	backtrack = func(nums []int, sub []int, sum int, start int) {
+		if sum > target {
+			return
+		}
+		if sum == target {
+			item := make([]int, 0)
+			item = append(item, sub...)
+			res = append(res, item)
 			return
 		}
 
-		for i := start; i < len(candidates); i++ {
-			if curSum+candidates[i] <= target {
-				subArr = append(subArr, candidates[i])
-				// start传入i, 保证下层节点与当前节点从同一个位置开始，避免结果重复 (如：[2,2,3]、[2,3,2]、[3,2,2])
-				backtract(i, curSum+candidates[i], candidates, subArr)
-				subArr = subArr[:len(subArr)-1]
-			}
+		for i := start; i < len(nums); i++ {
+			sub = append(sub, nums[i])
+			sum += nums[i]
+			// start传入i, 保证下层节点与当前节点从同一个位置开始，避免结果重复 (如：[2,2,3]、[2,3,2]、[3,2,2])
+			// 组合问题中 在回溯函数中需要传入起始位置，若数组中的元素可以重复使用，则向下层函数传入当前位置i,
+			// 若数组中的元素可以重复使用，则向下层函数传入当前位置i的下一个位置，即i+1
+			backtrack(nums, sub, sum, i)
+			sub = sub[:len(sub)-1]
+			sum -= nums[i]
 		}
 	}
 
-	backtract(0, 0, candidates, []int{})
+	backtrack(candidates, []int{}, 0, 0)
 	return res
 }
