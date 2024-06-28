ebc9de2eeaef2a86b7814fa8322b9a64bdbc13ed
jintian <jintianwang520@gmail.com>
2024-06-28 17:14:30 +0800

    0148 go

--- 0148_Sort_List/go/solution_2.go
+++ 0148_Sort_List/go/solution_2.go
@@ -1,8 +1,8 @@
 package main
 
-// 思路: 链表归并排序
+// 思路: 链表 归并排序 自顶向下递归式
 
-// 时间复杂度: O()    空间复杂度: O()
+// 时间复杂度: O(nlgn)    空间复杂度: O(lgn) 递归栈空间
 
 // Definition for singly-linked list.
 type ListNode struct {
@@ -11,7 +11,55 @@ type ListNode struct {
 }
 
 func sortList_2(head *ListNode) *ListNode {
-	// TODO
+	if head == nil || head.Next == nil {
+		return head
+	}
 
-	return nil
+	// 快慢指针 将链表均分成两部分
+	// 这里fast必须取head.Next, 若取了head, 则在链表节点为偶数时，切分不均匀，导致时间复杂度和空间复杂度上升
+	// 或者这里先加个虚拟头结点dummyHead, 可以令slow, fast := dummyHead, dummyHead, 效果也是均分的
+	slow, fast := head, head.Next
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	left, right := head, slow.Next
+	slow.Next = nil // 断成两部分
+
+	leftList := sortList_2(left)
+	rightList := sortList_2(right)
+	return merge(leftList, rightList)
+}
+
+func merge(l1, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+
+	dummyHead := new(ListNode)
+	cur := dummyHead
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			cur.Next = l1
+			l1 = l1.Next
+
+		} else {
+			cur.Next = l2
+			l2 = l2.Next
+		}
+
+		cur = cur.Next
+	}
+
+	if l1 != nil {
+		cur.Next = l1
+	}
+	if l2 != nil {
+		cur.Next = l2
+	}
+
+	return dummyHead.Next
 }
---
+++ 0148_Sort_List/go/solution_3.go
@@ -0,0 +1,17 @@
+package main
+
+// 思路: 链表 归并排序 自底向上迭代式 空间复杂度更优
+
+// 时间复杂度: O(nlgn)    空间复杂度: O(1)
+
+// Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// TODO
+func sortList_3(head *ListNode) *ListNode {
+
+	return nil
+}
