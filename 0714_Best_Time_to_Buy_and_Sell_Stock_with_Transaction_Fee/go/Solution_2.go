70672b8c2a60b43b0ddaf2e2a8bad9e35aaa33dd
jintian <jintianwang520@gmail.com>
2024-07-01 23:57:31 +0800

    0714 go

--- 0714_Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee/go/Solution_2.go
+++ 0714_Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee/go/Solution_2.go
@@ -1,27 +1,26 @@
 package main
 
-// 动态规划, 当天状态只与前一天的状态有关，使用两个变量即可维护转移关系
+// 思路：动态规划, 当天状态只与前一天的状态有关，使用两个变量即可维护转移关系
+
 // 时间复杂度：O(n), 空间复杂度：O(1)
 
-func maxProfit_2(prices []int, fee int) int {
-	n := len(prices)
-
-	// 状态初始化
-	sell := 0
-	buy := -prices[0]
-
-	// 状态转移，等号左边表示当天的状态，等号右边表示前一天的状态
-	for i := 1; i < n; i++ {
-		sell = max_2(sell, buy+prices[i]-fee)
-		buy = max_2(buy, sell-prices[i])
+func maxProfit(prices []int, fee int) int {
+	// dp0: 表示当天结束时，不持有股票可获得的最大利润
+	// dp1: 表示当天结束时，持有股票可获得的最大利润
+	// 在每次卖出时提交手续费
+	dp0, dp1 := 0, -prices[0]
+	for i := 1; i < len(prices); i++ {
+		dp0 = max(dp0, dp1+prices[i]-fee)
+		dp1 = max(dp1, dp0-prices[i]) // 由于没有冷冻期， 这里依赖的dp0直接去上步计算得到的值，可以理解为当天可以买出在买入
 	}
 
-	return sell
+	return dp0
 }
 
-func max_2(i, j int) int {
-	if i > j {
-		return i
+func max(a, b int) int {
+	if a > b {
+		return a
 	}
-	return j
+
+	return b
 }
