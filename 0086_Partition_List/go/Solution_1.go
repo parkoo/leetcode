7a38284a57ea2a477978ca5a4f74291fcde78cbc
jintian <jintianwang520@gmail.com>
2024-07-08 11:44:16 +0800

    0086 go

--- 0086_Partition_List/go/Solution_1.go
+++ 0086_Partition_List/go/Solution_1.go
@@ -1,6 +1,7 @@
 package main
 
-// 虚拟头节点 选取分区法
+// 思路：虚拟头节点 选取分区法 （将一个链表分成两个新链表，在将两个链表合并）
+
 // 时间复杂度：O(n)  空间复杂度：O(1)
 
 // Definition for singly-linked list.
@@ -13,18 +14,22 @@ func partition_1(head *ListNode, x int) *ListNode {
 	// 两个虚拟头节点 对链表进行选取分区
 	small, large := new(ListNode), new(ListNode)
 	curSmall, curLarge := small, large
+
 	for head != nil {
 		if head.Val < x {
 			curSmall.Next = head
 			curSmall = curSmall.Next
+
 		} else {
 			curLarge.Next = head
 			curLarge = curLarge.Next
 		}
+
 		head = head.Next
 	}
 
 	curSmall.Next = large.Next
 	curLarge.Next, large = nil, nil
+
 	return small.Next
 }
