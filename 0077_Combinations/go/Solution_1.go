babbc33bc281f2970cc6590771bc1a345deb1bd0
jintian <jintianwang520@gmail.com>
2024-07-25 17:37:08 +0800

    0077 go

--- 0077_Combinations/go/Solution_1.go
+++ 0077_Combinations/go/Solution_1.go
@@ -1,13 +1,14 @@
 package main
 
-// 组合问题　回溯 + 剪枝
+// 思路：组合问题　回溯
+
 // 时间复杂度：O(k*Cₙᵏ)  空间复杂度：O(Cₙᵏ)  其中,k表示需要将每个长度为k的组合都添加到输出中
-                                    
+
 var res [][]int
 
 func combine_1(n int, k int) [][]int {
 	res = [][]int{}
-	if n<k || n==0 || k==0 {
+	if n < k || n == 0 || k == 0 {
 		return res
 	}
 
@@ -17,15 +18,15 @@ func combine_1(n int, k int) [][]int {
 }
 
 func getCombinations(n int, k int, start int, cnt int, t []int) {
-	if cnt==k {
+	if cnt == k {
 		s := make([]int, len(t))
 		copy(s, t)
 		res = append(res, s)
-		return 
+		return
 	}
 
-	// 剪枝："i<=n"  -->  "i<=n-(k-cnt)+1" 
-	for i:=start; i<=n-(k-cnt)+1; i++ {  
+	// 剪枝："i<=n"  -->  "i<=n-(k-cnt)+1"
+	for i := start; i <= n-(k-cnt)+1; i++ {
 		t = append(t, i)
 		getCombinations(n, k, i+1, cnt+1, t)
 		t = t[:len(t)-1]
