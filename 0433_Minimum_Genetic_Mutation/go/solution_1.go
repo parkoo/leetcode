27a30c8f5116aeaa02121eb5927c51e20428225d
jintian <jintianwang520@gmail.com>
2024-07-25 17:39:24 +0800

    0433 go

---
+++ 0433_Minimum_Genetic_Mutation/go/solution_1.go
@@ -0,0 +1,48 @@
+package main
+
+// 思路: BFS
+
+// 时间复杂度: O(mn)    空间复杂度: O(mn)
+
+func minMutation_1(startGene string, endGene string, bank []string) int {
+
+	// 判断基因a是否经过一次转换可以变成基因b
+	canMutate := func(a, b string) bool {
+		diffCnt := 0
+		for i := 0; i < len(a); i++ {
+			if a[i] != b[i] {
+				diffCnt++
+			}
+		}
+
+		return diffCnt == 1
+	}
+
+	// bfs
+	visited := make(map[string]bool)
+	q := make([]string, 0)
+	q = append(q, startGene)
+
+	step := 0
+	for len(q) > 0 {
+		cnt := len(q)
+
+		for i := 0; i < cnt; i++ {
+			cur := q[0]
+			q = q[1:]
+			if cur == endGene {
+				return step
+			}
+
+			for _, next := range bank {
+				if canMutate(cur, next) && !visited[next] {
+					q = append(q, next)
+					visited[next] = true
+				}
+			}
+		}
+		step++
+	}
+
+	return -1
+}
