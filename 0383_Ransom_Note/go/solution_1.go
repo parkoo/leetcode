5ce09d289366a30be914868d343d5dfe0975d0f3
jintian <jintianwang520@gmail.com>
2024-07-26 09:01:16 +0800

    0383 go

---
+++ 0383_Ransom_Note/go/solution_1.go
@@ -0,0 +1,23 @@
+package main
+
+// 思路: 频率统计
+
+// 时间复杂度: O(m+n)    空间复杂度: O(n)
+
+func canConstruct_1(ransomNote string, magazine string) bool {
+	ss, tt := []rune(ransomNote), []rune(magazine)
+	tFreq := make(map[rune]int)
+	for i := 0; i < len(tt); i++ {
+		tFreq[tt[i]]++
+	}
+
+	for _, c := range ss {
+		if cnt, ok := tFreq[c]; !ok || cnt == 0 {
+			return false
+		}
+
+		tFreq[c]--
+	}
+
+	return true
+}
