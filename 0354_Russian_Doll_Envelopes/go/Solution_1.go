426af526e23c4006b78667702939969e79351c7f
jintian <jintianwang520@gmail.com>
2024-07-29 22:19:06 +0800

    0354 go

--- 0354_Russian_Doll_Envelopes/go/Solution_1.go
+++ 0354_Russian_Doll_Envelopes/go/Solution_1.go
@@ -4,13 +4,24 @@ import (
 	"sort"
 )
 
-// 动态规划 二维最长递增子序列问题
+// 思路： 动态规划
+
+// 信封的宽为w, 高为h
+// 首先我们将所有的信封按照 w 值第一关键字升序、h 值第二关键字降序进行排序
+// 按照上述方式排序后，我们就可以忽略 w 维度
+// 因为 对于 i > j, 一定有 w[i] >= w[j] w不在是限制只需要考虑h， 而当w[i] == w[j]时，h[i] < h[j], 即排除了将宽度相等的信封进行嵌套的情况
+// 因此，最终求出 h 维度的最长严格递增子序列，其长度即为答案
+
+// 经过排序后可以转化为对信封高度h求最长递增子序列问题 即同lc0300
+
 // 时间复杂度：O(n^2)  空间复杂度：O(n)
 
 func maxEnvelopes_1(envelopes [][]int) int {
+
 	// 排序
 	sort.Slice(envelopes, func(i int, j int) bool {
-		return envelopes[i][0] < envelopes[j][0] || (envelopes[i][0] == envelopes[j][0] && envelopes[i][1] > envelopes[j][1])
+		return envelopes[i][0] < envelopes[j][0] ||
+			(envelopes[i][0] == envelopes[j][0] && envelopes[i][1] > envelopes[j][1])
 	})
 
 	n := len(envelopes)
--- 0354_Russian_Doll_Envelopes/go/Solution_2.go
+++ 0354_Russian_Doll_Envelopes/go/Solution_2.go
@@ -4,30 +4,53 @@ import (
 	"sort"
 )
 
-// 贪心 + 二分
+// 思路： 贪心 + 二分
+
+// 经过排序处理后 转化为lc0300
+
 // 时间复杂度：O(nlgn)  空间复杂度：O(n)
 
 func maxEnvelopes_2(envelopes [][]int) int {
+	n := len(envelopes)
+	if n == 0 {
+		return 0
+	}
+
 	sort.Slice(envelopes, func(i, j int) bool {
-		return envelopes[i][0] < envelopes[j][0] || (envelopes[i][0] == envelopes[j][0] && envelopes[i][1] > envelopes[j][1])
+		return envelopes[i][0] < envelopes[j][0] ||
+			(envelopes[i][0] == envelopes[j][0] && envelopes[i][1] > envelopes[j][1])
 	})
 
-	n := len(envelopes)
-	// 用len(dp)表示最长的递增子序列
-	// dp 索引从0开始，取dp[i]时，len(dp)==i+1, 以dp[i] 记录长度为i+1的递增子序列的末尾元素的最小值
-	dp := make([]int, 0)
-	dp = append(dp, envelopes[0][1])
+	d := make([]int, n+1)
+	maxLen := 1
+	d[maxLen] = envelopes[0][1]
 
 	for i := 1; i < n; i++ {
-		e := envelopes[i][1]
-		if e > dp[len(dp)-1] {
-			dp = append(dp, e)
+		curHeigh := envelopes[i][1]
+		if curHeigh > d[maxLen] {
+			maxLen++
+			d[maxLen] = curHeigh
+
 		} else {
-			// 若dp中不存在元素e, 则返回元素e应该插入的位置
-			k := sort.SearchInts(dp, e)
-			dp[k] = e
+			index := binarySearch(d, maxLen, curHeigh)
+			d[index] = curHeigh
 		}
 	}
 
-	return len(dp)
+	return maxLen
+}
+
+func binarySearch(arr []int, maxLen int, target int) int {
+	left, right := 1, maxLen
+	for left < right {
+		mid := left + (right-left)/2
+		if arr[mid] >= target {
+			right = mid
+
+		} else {
+			left = mid + 1
+		}
+	}
+
+	return left
 }
