a56e92877f908893767a321aabb07b4499f48061
jintian <jintianwang520@gmail.com>
2024-08-27 00:07:38 +0800

    0470 go

---
+++ 0470_Implement_Rand10_Using_Rand7/go/solution_1.go
@@ -0,0 +1,22 @@
+package main
+
+// 思路:  较为通用， 但不是最优
+// https://leetcode.cn/problems/implement-rand10-using-rand7/solutions/979495/mo-neng-gou-zao-fa-du-li-sui-ji-shi-jian-9xpz/
+
+// 时间复杂度: O(?)    空间复杂度: O(?)
+
+func rand10_1() int {
+	first, second := rand7(), rand7()
+	for first > 6 {
+		first = rand7()
+	}
+	for second > 5 {
+		second = rand7()
+	}
+
+	if first%2 == 0 {
+		return second
+	} else {
+		return 5 + second
+	}
+}
